c884bffd4967210f3c77cd10957ad00e35b77aa0
Collins-cl <collinsolads@gmail.com>
2023-07-15 21:09:03 +0100

    finished responsiveness for heropage

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,15 +1,26 @@
 import React from "react";
 import Hero from "../Components/Hero/Hero";
+import Images from "../Variable/Images";
+import "../Pages/Home.scss";
 
 const Home = () => {
   return (
     <div>
       <Hero />
-      <p>xxx</p>
-      <p>xxx</p>
-      <p>xxx</p>
-      <p>xxx</p>
-      <p>xxx</p>
+
+      <div className="images-carousel">
+        <p>POWERED BY</p>
+        <div className="carousel">
+          <div className="card">
+            {Images.map((image) => (
+              <div key={image.id}>
+                <img src={image.image} width={120} alt="" />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+
       <p>xxx</p>
       <p>xxx</p>
       <p>xxx</p>
---
+++ src/Variable/Images.jsx
@@ -0,0 +1,84 @@
+const Images = [
+  {
+    id: 1,
+    image: "/src/assets/partners/certik.webp",
+  },
+  {
+    id: 2,
+    image: "/src/assets/partners/circle.webp",
+  },
+  {
+    id: 3,
+    image: "/src/assets/partners/frakt.webp",
+  },
+  {
+    id: 4,
+    image: "/src/assets/partners/francium.webp",
+  },
+  {
+    id: 5,
+    image: "/src/assets/partners/hubble.webp",
+  },
+  {
+    id: 6,
+    image: "/src/assets/partners/kamino.webp",
+  },
+  {
+    id: 7,
+    image: "/src/assets/partners/lido.webp",
+  },
+  {
+    id: 8,
+    image: "/src/assets/partners/marinade.webp",
+  },
+  {
+    id: 9,
+    image: "/src/assets/partners/mercuial.webp",
+  },
+  {
+    id: 10,
+    image: "/src/assets/partners/orca.webp",
+  },
+  {
+    id: 11,
+    image: "/src/assets/partners/port.webp",
+  },
+  {
+    id: 12,
+    image: "/src/assets/partners/quarry.webp",
+  },
+  {
+    id: 13,
+    image: "/src/assets/partners/mercuial.webp",
+  },
+  {
+    id: 14,
+    image: "/src/assets/partners/orca.webp",
+  },
+  {
+    id: 15,
+    image: "/src/assets/partners/port.webp",
+  },
+  {
+    id: 177,
+    image: "/src/assets/partners/quarry.webp",
+  },
+  {
+    id: 18,
+    image: "/src/assets/partners/saber.webp",
+  },
+  {
+    id: 19,
+    image: "/src/assets/partners/serum.webp",
+  },
+  {
+    id: 20,
+    image: "/src/assets/partners/solanafm.webp",
+  },
+  {
+    id: 21,
+    image: "/src/assets/partners/solend.webp",
+  },
+];
+
+export default Images;
